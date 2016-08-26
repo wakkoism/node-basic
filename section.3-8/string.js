a1d449d2c9ddc01634e99bc4a1be037ec4bc6820
John Le <john.le@bonniercorp.com>
2016-08-26 12:06:09 -0400

    Completed section 3-8

---
+++ section.3-8/string.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var
+  firstName = 'John',
+  lastName = 'Doe',
+  containsString = ' number of characters.';
+
+console.log(firstName + ' ' + lastName);
+console.log(firstName + ' contains ' + firstName.length + containsString);
+console.log(lastName + ' contains ' + lastName.length + containsString);
