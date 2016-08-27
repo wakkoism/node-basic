67ea8117bf7fceaf474851e149861bf949daf78a
John Le <john.le@bonniercorp.com>
2016-08-27 16:09:25 -0400

    Complee section 3-10

---
+++ section.3-10/if.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var friend = 'Moo Cow';
+var myFriends = ['Mike', 'John', 'Mark'];
+var person = 'Josh';
+if (myFriends.indexOf(person ) > 0) {
+  console.log('Hello friend');
+}
+else if (person === 'Josh') {
+  console.log('Hello');
+}
+else {
+  console.log('Hello stranger');
+}
+
+
+
