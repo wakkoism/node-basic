dd61125a8f8de0f3c875f0e92094d08758e31031
John Le <john.le@bonniercorp.com>
2017-11-25 08:44:07 -0500

    7-54: Finish the todo by params s

--- todo-api/server.js
+++ todo-api/server.js
@@ -1,12 +1,47 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 3000;
+const todos = [
+  {
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false,
+  },
+  {
+    id: 2,
+    description: 'Go to market',
+    completed: false,
+  },
+  {
+    id: 3,
+    description: 'This is a challenge request',
+    completed: true,
+  },
+];
+// Function that finds the key value pair inside of the array of objects.
+const getKeyByValue = (object, key, value) => object.find(element => element[key] === value);
 
 app.get('/', (request, response) => {
   response.send('Todo API Root');
 });
 
+app.get('/todos', (request, response) => {
+  response.json(todos);
+});
+
+app.get('/todo/:id', (request, response) => {
+  const { id } = request.params;
+
+  const todo = getKeyByValue(todos, 'id', Number(id));
+  if (todo) {
+    response.json(todo);
+  } else {
+    response.status(404).send();
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Express lisening on port ${port}!`);
-})
+  console.log(`Express listening on port ${port}!`);
+});
+
